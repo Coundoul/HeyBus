24e74acb7335e31f2d1ec9bd5746abaa00b82e4c
Ibrahima thiam <ibrahima1.thiam@uvs.edu.sn>
2021-06-23 12:23:18 +0000

    home page

--- src/main/webapp/app/entities/transporteur/transporteur.module.ts
+++ src/main/webapp/app/entities/transporteur/transporteur.module.ts
@@ -6,9 +6,11 @@ import { TransporteurDetailComponent } from './detail/transporteur-detail.compon
 import { TransporteurUpdateComponent } from './update/transporteur-update.component';
 import { TransporteurDeleteDialogComponent } from './delete/transporteur-delete-dialog.component';
 import { TransporteurRoutingModule } from './route/transporteur-routing.module';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [SharedModule, TransporteurRoutingModule],
+  imports: [SharedModule, TransporteurRoutingModule, MatStepperModule, MatIconModule],
   declarations: [TransporteurComponent, TransporteurDetailComponent, TransporteurUpdateComponent, TransporteurDeleteDialogComponent],
   entryComponents: [TransporteurDeleteDialogComponent],
 })
